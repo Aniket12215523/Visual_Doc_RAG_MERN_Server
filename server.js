7952de9994768889fc1a467d76bdd6b452606aee
Aniket12215523 <115388792+Aniket12215523@users.noreply.github.com>
2025-08-14 22:04:32 +0530

    Refactored server file for cors error

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
   ],
   credentials: true, 
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-session-id']
 }));
 
 app.options('*', cors());
